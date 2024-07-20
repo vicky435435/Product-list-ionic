e744dfc7b08bd0438bc0499a69edcb27e51f0ccc
ramkrushna30 <ramkrushna.maneshwar@thinkersteps.com>
2024-07-20 15:03:49 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Product',
+  webDir: 'www'
+};
+
+export default config;
